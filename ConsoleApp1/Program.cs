26c29069c718faa0535adfb3c5f55ef17b2ae758
gerikeee69 <gerike.nemeth@gmail.com>
2024-10-25 13:02:22 +0200

    24.10.25

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace korhenger
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kor k1 = new kor();
+            k1.SetSugar(15);
+            k1.SetTerulet();
+            k1.SetKerulet();
+
+            kiir(k1);
+
+
+            kor k2 = new kor(58);
+            k2.SetKerulet();
+            k2.SetTerulet();
+
+            kiir(k2);
+
+            Console.ReadKey();
+        }
+
+        private static void kiir(kor k)
+        {
+            Console.WriteLine($"A{k.GetSugar()}sugaru kor kerulet; {k.GetKerulet()},terulete pedig {k.GetTerulet()}");
+
+        }
+    }
+}
---
+++ ConsoleApp1/kor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace korhenger
+{
+    class kor
+    {
+        private double sugar,terulet,kerulet;
+
+        public kor() { }
+
+            public kor(double r)
+            {
+            sugar = r;
+            }
+
+        public void SetSugar(double r)
+        {
+            sugar = r;
+        }
+
+        public void SetTerulet()
+        {
+            this.terulet = Math.Round (Math.Pow(this.sugar, 2) * Math.PI);
+        }
+
+        public void SetKerulet()
+        {
+            this.kerulet = Math.Round ( 2 * this.sugar * Math.PI);
+        }
+        
+        public double GetTerulet()
+        {
+            return this.terulet;
+        }
+        public double GetKerulet()
+        {
+            return this.kerulet;
+        }
+
+        public double GetSugar()
+        {
+            return this.sugar;
+        }
+
+    }
+}
